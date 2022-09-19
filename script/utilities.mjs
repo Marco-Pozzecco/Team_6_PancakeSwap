b17bc6ef91de61739541dc80371f58bfd35f0488
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-09-19 23:43:53 +0200

    added randomize module

--- script/utilities.mjs
+++ script/utilities.mjs
@@ -1,11 +1,11 @@
 export default class Utilities {
     // Accetta una stringa che rappresenta un selettore CSS, ritorna tutti gli elementi che soddisfano quel selettore
-    getAllElements(str) {
+    static getAllElements(str) {
         return document.querySelectorAll(str);
     }
 
     // Accetta una stringa che rappresenta un selettore CSS, ritorna il primo elemento che soddisfa quel selettore
-    getElement(str) {
+    static getElement(str) {
         return document.querySelector(str);
     }
 
