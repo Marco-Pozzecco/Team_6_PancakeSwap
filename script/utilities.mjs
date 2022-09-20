8c25d0a197a223199edd6bcdc0797c5d7195123a
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-09-20 11:15:46 +0200

    fix

--- script/utilities.mjs
+++ script/utilities.mjs
@@ -1,14 +1,13 @@
+export default class Utilities {
+    // Accetta una stringa che rappresenta un selettore CSS, ritorna tutti gli elementi che soddisfano quel selettore
+    getAllElements(str) {
+        return document.querySelectorAll(str);
+    }
 
-// export default class Utilities {
-//     // Accetta una stringa che rappresenta un selettore CSS, ritorna tutti gli elementi che soddisfano quel selettore
-//     getAllElements(str) {
-//         return document.querySelectorAll(str);
-//     }
-
-//     // Accetta una stringa che rappresenta un selettore CSS, ritorna il primo elemento che soddisfa quel selettore
-//     getElement(str) {
-//         return document.querySelector(str);
-//     }
+    // Accetta una stringa che rappresenta un selettore CSS, ritorna il primo elemento che soddisfa quel selettore
+    getElement(str) {
+        return document.querySelector(str);
+    }
 
 
-// }
+}
