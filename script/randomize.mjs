e4605ef6330d2e4090079bd8f5614ebc45e16117
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-09-20 14:48:17 +0200

    update console.log randomize

--- script/randomize.mjs
+++ script/randomize.mjs
@@ -2,9 +2,7 @@ export default class Randomize {
     // Genera dei valori (numbers) casuali entro un valore massimo ed uno minimo ad ogni intervallo di tempo
     static randomNumInRange(minValue, maxValue) {
         let k = Math.random(); // Coefficiente da 0 a 1
-        console.log(k);
         let range = (maxValue - minValue); // Calcolo del range
-        console.log(minValue + range * k);
         return Math.round(minValue + range * k)  // ritorna il k% del range 
     }
     // Chiama una funzione ogni intervallo di tempo
